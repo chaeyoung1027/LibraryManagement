424b68f9ec697f87e70c50764a6ff762e0b23f05
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-12-03 00:05:54 +0900

    도서관 매뉴얼 페이지

---
+++ LibraryManual.py
@@ -0,0 +1,39 @@
+# 도서를 관리하는 콘솔 프로그램
+# 도서 정보는 도서명, 저자로 구성
+import Search
+from AllBook import AllBook
+from Search import search_book
+import AddBook
+import DelBook
+
+
+class Library:
+    def __init__(self):
+        self.BookList = []  # 책의 목록을 저장할 리스트
+
+    def library_manual(self):
+        while True:
+            self.show_manual()
+            inp = input("원하시는 기능을 선택하세요: ")
+            if inp == "1":
+                AllBook.AllBook()
+            elif inp == "2":
+                Search.search_book()
+            elif inp == "3":
+                book = AddBook()
+                AddBook.append(book)
+            elif inp == "4":
+                DelBook
+            elif inp == "5":
+                print("\n도서 찾기 서비스를 종료합니다.")
+                break
+
+    def show_manual(self):
+        print("도서 관리 프로그램입니다.")
+        print("*" * 17)
+        print("1. 도서 목록 보기")
+        print("2. 도서 검색하기")
+        print("3. 도서 추가하기")
+        print("4. 도서 삭제하기")
+        print("5. 종료하기")
+        print("*" * 17)
