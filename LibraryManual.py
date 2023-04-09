35d36b38f0108fe4c3501d0d432ed7e076756641
Chaeyoung Lim <rnldyal1027@naver.com>
2023-04-10 08:53:47 +0900

    change to function

--- LibraryManual.py
+++ LibraryManual.py
@@ -6,13 +6,24 @@ from DelBook import DeleteBook
 from SearchBook import SearchBook
 
 
+def show_manual():
+    print(f'\n도서 관리 프로그램입니다.')
+    print("*" * 17)
+    print(f'1. 도서 목록 보기')
+    print(f'2. 도서 검색하기')
+    print(f'3. 도서 추가하기')
+    print(f'4. 도서 삭제하기')
+    print(f'5. 종료하기')
+    print("*" * 17)
+
+
 class Library:
     def __init__(self):
         self.BookList = []  # 책 목록
 
     def library_manual(self):
         while True:
-            self.show_manual()
+            show_manual()
             inp = input(f'원하시는 기능을 선택하세요: ')
             if inp == "1":
                 AllBook(self.BookList)
@@ -26,15 +37,6 @@ class Library:
                 print(f'\n도서 찾기 서비스를 종료합니다.')
                 break
 
-    def show_manual(self):
-        print(f'\n도서 관리 프로그램입니다.')
-        print("*" * 17)
-        print(f'1. 도서 목록 보기')
-        print(f'2. 도서 검색하기')
-        print(f'3. 도서 추가하기')
-        print(f'4. 도서 삭제하기')
-        print(f'5. 종료하기')
-        print("*" * 17)
 
 if __name__ == "__main__":
     Library = Library()
